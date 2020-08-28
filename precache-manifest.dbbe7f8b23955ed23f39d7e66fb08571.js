bebf75a970f3e10af95e136760742e21af46d605
Krishnakumar <krishkumarv2001@gmail.com>
2020-08-29 00:28:22 +0530

    Updates

---
+++ precache-manifest.dbbe7f8b23955ed23f39d7e66fb08571.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9970346f126cdeded340d8858dbe999",
+    "url": "/vedictheology/index.html"
+  },
+  {
+    "revision": "8e56fa07c19f81fde753",
+    "url": "/vedictheology/static/css/2.97ca6098.chunk.css"
+  },
+  {
+    "revision": "9dde08a2fbef691f3513",
+    "url": "/vedictheology/static/css/main.3dedce71.chunk.css"
+  },
+  {
+    "revision": "8e56fa07c19f81fde753",
+    "url": "/vedictheology/static/js/2.0689fdf2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/vedictheology/static/js/2.0689fdf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dde08a2fbef691f3513",
+    "url": "/vedictheology/static/js/main.0ded2a8e.chunk.js"
+  },
+  {
+    "revision": "47bb91b8330b5d5f5e1a",
+    "url": "/vedictheology/static/js/runtime-main.ca17873b.js"
+  }
+]);
